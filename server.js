f34c00899d3975e593404189039972337ba9aede
Adam Olson <adamlsn@gmail.com>
2021-05-16 19:08:37 -0500

    tweaks to htmlRoutes and server.js, adding heroku

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-const {notes} = require("./db/db.json");
 const apiRoutes = require("./routes/apiRoutes/apiRoutes")
 const htmlRoutes = require("./routes/htmlRoutes/htmlRoutes")
-const {newNote, deleteNote} = require("./lib/notes");
-const {v4: uuidv4} = require("uuid");
 
-const path = require("path");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
