2c100fc06ef5f01f1db22475e9ee76fe58c41879
Adam Olson <adamlsn@gmail.com>
2021-05-22 12:20:10 -0500

    trying to fix 'Push' error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
 
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
